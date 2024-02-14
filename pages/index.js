6d5f89b7afbb3237bccad9426b02e524bfa701d3
devMuangi <devmuangi@gmail.com>
2024-02-14 17:01:20 +0300

    init

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,22 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="container">
-      <Head>
+      {/* <Head>
         <title>comido school eldoret</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </Head> */}
+
+<Head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+  <meta name="description" content="Comido school application" />
+  <meta name="theme-color" content="#000" />
+  <title>Comido | School | Eldoret</title>
+  <link rel="manifest" href="/manifest.json" />
+  <link rel="shortcut icon" href="/favicon.ico" />
+  <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+</Head>;
 
       <main>
         <Header title="Welcome to Comido school Eldoret" />
